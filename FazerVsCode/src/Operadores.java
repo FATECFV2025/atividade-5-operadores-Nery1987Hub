8b9ca35f579d7a6d7db1b000abc682c7f52004d2
Nery1987 <filipelimanery@gmail.com>
2025-12-07 15:15:30 -0300

    Construindo classe Operadores no arquivo "Operadores.java"

---
+++ FazerVsCode/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public float adicao (float x, float y)
+    {
+        return x + y;
+    }
+
+    public float subt (float x, float y)
+    {
+        return x - y;
+    }
+
+    public float multi (float x, float y)
+    {
+        return x - y;
+    }
+
+    public float divis (float x, float y)
+    {
+        return x - y;
+    }
+}
