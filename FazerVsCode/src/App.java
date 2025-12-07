25bf8f4ba80754f91ba720ed480ebb75a5589ec0
Nery1987 <filipelimanery@gmail.com>
2025-12-07 15:50:25 -0300

    Ajustando saídas do terminal e inserindo saída dos métodos dos operadores de atribuição em "App.java"

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -6,15 +6,24 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Insira um valor para X: ");
         float x = sc.nextInt();
 
-        System.out.println();
+        System.out.println("Digite um valor para Y: ");
         float y = sc.nextInt();
 
-        System.out.println(op.adicao(x, y));
-        System.out.println(op.subt(x, y));
-        System.out.println(op.multi(x, y));
-        System.out.println(op.divis(x, y));
+        System.out.println("Adição" + op.adicao(x, y));
+        System.out.println("Subtração" + op.subt(x, y));
+        System.out.println("Divisão" + op.multi(x, y));
+        System.out.println("Multiplicação" + op.divis(x, y));
+        System.out.println("_________________");
+
+        System.out.println("X += Y: " + op.maisIgual(x, y));
+        System.out.println("X -= Y: " + op.menosIgual(x, y));
+        System.out.println("X *= Y: " + op.vezesIgual(x, y));
+        System.out.println("X /= Y: " + op.divideIgual(x, y));
+        System.out.println("_________________");
+
+        
     }
 }
